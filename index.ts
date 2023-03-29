ee055bcf88ee3a1d34baf960f7924ddf57086079
fabio dasilva de oliveira <dasilvadeoliveirafabio@gmail.com>
2023-03-29 15:57:57 -0300

    teste de deploy 6

--- index.ts
+++ index.ts
@@ -4,10 +4,10 @@ import { router } from "./src/routes";
 const app =  express()
 app.use(cors())
 app.use(express.json())
-// router.forEach(elem=>{
-//     return app.use(elem.endpoint, elem.route)
-// })
-app.get("/",(req,res)=>{
-   res.send("olaaa")
+router.forEach(elem=>{
+    return app.use(elem.endpoint, elem.route)
 })
+// app.get("/",(req,res)=>{
+//    res.send("olaaa")
+// })
 app.listen(4000,()=>console.log("rodando na porta 4000 ..."))
